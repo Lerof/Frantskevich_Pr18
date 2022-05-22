6043de058314a7bdc6c7105e2912671bb3cb8182
Леонид Францкевич <volt-2014@list.ru>
2022-05-22 21:54:21 +0300

    Добавьте файлы проекта.

---
+++ Frantskevich_Pr18/Frantskevich_Pr18/RickAndMorty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frantskevich_Pr18
+{
+    public class Origin
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Location
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class RicAndMorty
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string type { get; set; }
+        public string gender { get; set; }
+        public Origin origin { get; set; }
+        public Location location { get; set; }
+        public string image { get; set; }
+        public List<string> episode { get; set; }
+        public string url { get; set; }
+        public DateTime created { get; set; }
+    }
+}
